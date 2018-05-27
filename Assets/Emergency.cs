fabf90239ce69c87b285972af0b9e11832b96aed
caera-yc <38510526+caera-yc@users.noreply.github.com>
2018-05-28 09:37:42 +1200

    Initial commit

---
+++ Assets/Emergency.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+//Make sure you get rid of the namespace that is automatically generated here.
+class Emergency
+{
+    //List all the columns of your table here in the same format as below.
+    public string EmergencyTypes { get; set; }
+    public string Location { get; set; }
+
+}
